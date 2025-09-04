af72d16cbc80171cdf19a9606da74aeeaebb6f69
regilanmeira <regilan@hotmail.com>
2025-09-04 07:36:39 -0300

    Versão Inicial do Projeto

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="" method="post">
+        <h1>Cadastro de cliente</h1>
+        <ul>
+            <li>Nome</li>
+            <li><input type="text" name="txtNome" id=""></li>
+            <li>Telefone</li>
+            <li><input type="text" name="txtTelefone" id=""></li>
+            <li><button type="submit">Enviar</button></li>
+        </ul>
+    </form>
+</body>
+</html>
